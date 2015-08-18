41a01c48445f1dbc915df13d4052925fb0d5f748
deciomoritz <deciomoritz1@gmail.com>
2015-08-18 00:23:22 -0300

    Faltando um método da lista. Verificar

---
+++ CG1.cpp
@@ -0,0 +1,19 @@
+//============================================================================
+// Name        : CG1.cpp
+// Author      : 
+// Version     :
+// Copyright   : Your copyright notice
+// Description : Hello World in C++, Ansi-style
+//============================================================================
+
+#include <iostream>
+#include "include/ListaEnc.hpp"
+using namespace std;
+
+int main() {
+
+	ListaEnc<int> lista;
+
+	cout << "!!!Hello World!!!" << endl; // prints !!!Hello World!!!
+	return 0;
+}
