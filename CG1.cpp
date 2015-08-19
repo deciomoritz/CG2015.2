a7b76ed81d6d380edbe041e52749ffe8dbb03e2a
deciomoritz <deciomoritz1@gmail.com>
2015-08-19 01:55:58 -0300

    GTK adicionado, makefile adicionado

--- CG1.cpp
+++ CG1.cpp
@@ -1,13 +1,26 @@
 #include <iostream>
-
 #include "include/ListaEnc.hpp"
+
+#include <gtk-3.0/gtk/gtk.h>
 
 using namespace std;
 
-int main() {
+int main(int argc,char* argv[]) {
 
 	ListaEnc<int> lista();
 
-	cout << "!!!Hello World!!!" << endl; // prints !!!Hello World!!!
+	cout << "!!!Hello World!!!" << endl;
+
+	gtk_init(&argc, &argv);
+
+	GtkWidget* window = gtk_window_new(GTK_WINDOW_TOPLEVEL);
+
+	g_signal_connect (window, "destroy", G_CALLBACK (gtk_main_quit), NULL);
+
+	gtk_widget_show (window);
+
+
+	gtk_main();
+
 	return 0;
 }
---
+++ Makefile
@@ -0,0 +1,5 @@
+make all:
+	g++ `pkg-config --cflags gtk+-3.0` -o exec *.cpp `pkg-config --libs gtk+-3.0` -std=c++11 -rdynamic
+
+make run:
+	./exec
