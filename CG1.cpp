96e5a1ed582fad7237053346e8514d94a874eaea
deciomoritz <deciomoritz1@gmail.com>
2015-08-19 20:35:40 -0300

    Dois botões com GTK

--- CG1.cpp
+++ CG1.cpp
@@ -21,12 +21,24 @@ int main(int argc,char* argv[]) {
 	g_signal_connect (window, "destroy", G_CALLBACK (gtk_main_quit), NULL);
 	gtk_container_set_border_width (GTK_CONTAINER (window), 10);
 
+
 	GtkWidget* button = gtk_button_new_with_label("Meu botão");
 	gtk_widget_set_size_request(button, 30,10);
-
 	gtk_widget_show(button);
-	gtk_container_add(GTK_CONTAINER(window), button);
+
+
+	GtkWidget* button2 = gtk_button_new_with_label("Meu botão2");
+	gtk_widget_set_size_request(button2, 30,10);
+	gtk_widget_show(button2);
+
+	GtkWidget* grid = gtk_grid_new();
+	gtk_grid_attach(GTK_GRID(grid),button,0,0,30,10);
+	gtk_grid_attach(GTK_GRID(grid),button2,0,30,30,10);
+
+	gtk_container_add(GTK_CONTAINER(window), grid);
+
 	gtk_widget_show (window);
+	gtk_widget_show_all (window);
 	gtk_main();
 
 	return 0;
