b1e481e149b8b926fbf63e3f9f230f8a9cdb9452
luizhlc <luizhenriquecancellier@gmail.com>
2015-08-26 23:48:17 -0300

    criacao dos manipulas arrumada

--- include/ManipulaMatriz.hpp
+++ include/ManipulaMatriz.hpp
@@ -1,38 +1,39 @@
 #include <math.h>
+#include <iostream>
 #include "Coordenada.hpp"
+using namespace std;
 class ManipulaMatriz {
 private:
 	vector<vector<double> >translacaoBase, rotacaoBase, escalonamentoBase;
 public:
 	ManipulaMatriz():translacaoBase(3,vector<double>(3,0)), rotacaoBase(3,vector<double>(3,0)), escalonamentoBase(3,vector<double>(3,0)){
-
 		for(int i =0; i<3; i++){
 			translacaoBase[i][i]=1.0;
 
 		}
-		rotacaoBase[3][3]=1.0;
-		escalonamentoBase[3][3] =1.0;
+		rotacaoBase[2][2]=1.0;
+		escalonamentoBase[2][2] =1.0;
 	};
 	~ManipulaMatriz(){};
-		vector<vector<double>> getTranslacao(Coordenada deslocamento){
+		vector<vector<double> > getTranslacao(Coordenada deslocamento){
 			translacaoBase[0][3] = deslocamento.getX();
 			translacaoBase[1][3] = deslocamento.getY();
 			return translacaoBase;
 		}
-		vector<vector<double>> getEscalonamento(Coordenada fator){
+		vector<vector<double> > getEscalonamento(Coordenada fator){
 			escalonamentoBase[0][0] = fator.getX();
 			escalonamentoBase[1][1] = fator.getY();
 			return escalonamentoBase;
 		}
-		vector<vector<double>> getRotacao(double theta){
+		vector<vector<double> > getRotacao(double theta){
 			rotacaoBase[0][0] = cos(theta);
 			rotacaoBase[0][1] = -sin(theta);
 			rotacaoBase[1][0] = sin(theta);
 			rotacaoBase[1][1] = cos(theta);
 			return rotacaoBase;
 		}
-		vector<vector<double>> multiplicaMatriz(vector<vector<double>> a, vector<vector<double>> b){
-			vector<vector<double>> result(a.size(), vector<double>(b[0].size(),0));
+		vector<vector<double> > multiplicaMatriz(vector<vector<double> > a, vector<vector<double> > b){
+			vector<vector<double> > result(a.size(), vector<double>(b[0].size(),0));
 			for (int i = 0; i < a.size(); i++) {
 			      for (int j = 0; j < b[0].size(); j++) {
 			        for (int k = 0; k < b.size(); k++) {
